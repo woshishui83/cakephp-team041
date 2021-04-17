3cae4e85819e4b924a9ac6b4e174eef372d7178a
Mars <woshishui83@163.com>
2021-04-18 03:16:34 +0800

    login

---
+++ src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mars
+ * Date: 2021/4/18
+ * Time: 03:05
+ */
+
+namespace App\Controller;
+
+
+class UserController extends AppController {
+
+    /**
+     * 登录
+     */
+    public function login() {
+
+        $this->set();
+    }
+
+    /**
+     * 注册
+     */
+    public function reg() {
+
+        $this->set();
+    }
+}
---
+++ src/Template/User/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Home</title>
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/common.css" />
+		<link rel="stylesheet" href="css/index.css" />
+	</head>
+	<body>
+		<div class="top-bar primary-bg">
+			<img class="logo" src="images/WechatIMG6.png" />
+			<div class="title">DANUBE TRAVER AGENCY</div>
+			<div class="text">
+				<span>
+					<a href="./admin/login/login.php">Sign in / Register</a>
+				</span>
+			</div>
+		</div>
+		<div class="nav-box">
+			<div class="nav">
+				<a href="#">HOME</a>
+				<a href="#">SERVICES</a>
+				<a href="#">TRAVERING PLAN</a>
+				<a href="#">DESTINATION</a>
+				<a href="#">ACTIVITIES</a>
+				<a href="#">CONTACT US</a>
+				<span class="glyphicon glyphicon-search"></span>
+			</div>
+		</div>
+		<img class="banner" src="images/WechatIMG7.jpeg" />
+		<div class="bottom-bar primary-bg">
+			<div class="left">
+				<div class="title"><span>Travel Packages</span></div>
+				<div class="img-box">
+					<div class="left">
+						<img src="images/WechatIMG470.png" />
+						<div>Vietnam</div>
+					</div>
+					<div class="right">
+						<img src="images/WechatIMG469.png"/>
+						<div>Eastern europe</div>
+					</div>
+				</div>
+			</div>
+			<div class="right">
+				<div class="title"><span>Special offer</span></div>
+				<div class="img-box">
+					<div class="left">
+						<img src="images/WechatIMG468.png" />
+						<div>Slovak Spa</div>
+					</div>
+					<div class="right">
+						<img src="images/WechatIMG3.jpeg"/>
+						<div>Baltic</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
